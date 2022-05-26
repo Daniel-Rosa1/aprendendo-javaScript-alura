e217ace64ad34a0e8065fb68660b8188db9dfc05
Daniel-Rosa1 <57119617+Daniel-Rosa1@users.noreply.github.com>
2022-05-26 12:19:53 -0300

    termino do curso de java script para back-end

---
+++ cursoOO/objetoLiteral.js
@@ -0,0 +1,27 @@
+
+const user = {
+  nome : 'julia',
+  email: 'j@j.com',
+  nascimento: '2021/01/01',
+  role: 'admin',
+  ativo: true,
+
+  exibirInfos: function(){
+    console.log(this.nome, this.email)
+  }
+}
+
+const admin = {
+  nome : 'Marianan',
+  email: 'm@m.com',
+  role: 'admin',
+  criarCurso(){
+    console.log('curso criado')
+  }
+}
+
+Object.setPrototypeOf(admin, user)
+
+admin.criarCurso()
+admin.exibirInfos()
+
